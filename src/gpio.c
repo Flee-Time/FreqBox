493bc2e83b9ed6804cddf131932a2a6b179b3ffb
Flee-Time <koray509@gmail.com>
2024-07-30 17:43:33 +0300

    added blinking led to hardfault

--- src/gpio.c
+++ src/gpio.c
@@ -88,6 +88,7 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
 }
 
 /* USER CODE BEGIN 2 */
